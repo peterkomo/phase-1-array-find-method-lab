894221b111f34a5f05325cb79c67bab17922c506
peterkomo <mwangipeterkomo@gmail.com>
2023-06-28 14:29:17 +0300

    adde an array function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+// code your solution hefunction superbowlWin(arr) {
+    function superbowlWin(arr) {
+        const winningYear = arr.find(function(obj) {
+          return obj.result === "W";
+        });
+        return winningYear ? winningYear.year : undefined;
+      }
+      
